e2ee0dee84ade1b01602b2a5e636c835592a235a
StudyAnkit <ankitghosh2018@gmail.com>
2022-03-07 09:41:10 +0530

    Created a board for TicToeGame

---
+++ TicTacToeGame/TicTacTaeGame.cs
@@ -0,0 +1,20 @@
+﻿namespace TicTacToeGame
+{
+    internal class TicTacToeGame
+    {
+        public char[] CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                board[i] = ' '; 
+            }
+            return board;
+        }
+        public static void Main()
+        {
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateBoard();
+        }
+    }
+}
